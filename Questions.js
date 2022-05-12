2ff8b72e4d1e8ae513f8b285b44fe88968b07765
cesmoi <83175114+cesmoi@users.noreply.github.com>
2022-05-12 09:26:14 +0000

    Create Questions.js

---
+++ Questions.js
@@ -0,0 +1,221 @@
+questions = [
+    {
+        "question": "L'economie autrichienne est meilleur que l'économie marxiste.",
+        "effect": {
+            "econ": -10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0
+            "comm": 0
+        }
+    },
+    {
+        "question": "Le néolibéralisme est une bonne chose.",
+        "effect": {
+            "econ": -10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Les entreprises devraient exister.",
+        "effect": {
+            "econ": -10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 5
+        }
+    },
+    {
+        "question": "Il faut tout faire pour qu'un ne sombre pas dans le communisme même si il faudra tuer des gens.",
+        "effect": {
+            "econ": -10,
+            "civi": -5,
+            "dipl": 5,
+            "soci": -5,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Le libéralisme économique est essentiel.",
+        "effect": {
+            "econ": -10,
+            "civi": 5,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "L'economie marxiste est souhaitable.",
+        "effect": {
+            "econ": 10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": -5
+        }
+    },
+    {
+        "question": "Je préfère l'urss qu'au états-unis.",
+        "effect": {
+            "econ": 10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": -5
+        }
+    },
+    {
+        "question": "Tout le monde est ou devraient être socialiste par nature.",
+        "effect": {
+            "econ": 10,
+            "civi": 5,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Presque chaque nuit j'imagine à quelle la fin du capitalisme serait une bonne chose.",
+        "effect": {
+            "econ": 10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Le socialisme est la démocratie vont parfaitement ensemble.",
+        "effect": {
+            "econ": 10,
+            "civi": 5,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Les grèves doivent immédiatement s'arrêter!",
+        "effect": {
+            "econ": -10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 5
+        }
+    },
+  {
+        "question": "Je n'aime pas les syndicats.",
+        "effect": {
+            "econ": -10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 5
+        }
+    },
+    {
+        "question": "Le communisme n'ai rien d'autre qu'une erreur.",
+        "effect": {
+            "econ": -10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Ce n'est pas parce que les nazbols sont communistes qu'il ne sont pas racistes.",
+        "effect": {
+            "econ": -10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 5,
+            "comm": 0
+        }
+    },
+    {
+        "question": "C'est une bonne chose que le système féodale face place au système capitaliste.",
+        "effect": {
+            "econ": -10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "C'est une bonne chose que le système féodale fasse place au système capitaliste.",
+        "effect": {
+            "econ": -10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Les travailleurs ont besoin de prendre le contrôle de la production.",
+        "effect": {
+            "econ": 10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Les nazis ne sont pas communiste/socialiste et c'est une bonne chose.",
+        "effect": {
+            "econ": 10,
+            "civi": 5,
+            "dipl": 0,
+            "soci": 5,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Benito Mussolini n'aurait jamais était tué si il n'avait abandonner le socialisme.",
+        "effect": {
+            "econ": 10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Les anarcho-capitalistes ne sont pas de vrai anarchiste.",
+        "effect": {
+            "econ": 10,
+            "civi": 0,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Tuons les riches!",
+        "effect": {
+            "econ": 15,
+            "civi": 5,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Nous devons nous battre contre la dictature.",
+        "effect": {
+            "econ": 0,
+            "civi": 10,
+            "dipl": 0,
+            "soci": 5,
+            "comm": 0
+        }
+    },
