7506b68ab7a23344ef3fc6001857c71fd7934d50
cesmoi <83175114+cesmoi@users.noreply.github.com>
2022-05-15 12:22:19 +0000

    Update Questions.js

--- Questions.js
+++ Questions.js
@@ -219,3 +219,203 @@ questions = [
             "comm": 0
         }
     },
+    {
+        "question": "Le fascisme est mauvais.",
+        "effect": {
+            "econ": 0,
+            "civi": 10,
+            "dipl": 0,
+            "soci": 5,
+            "comm": 0
+        }
+    },
+    {
+        "question": "La violence est justifiée si elle est utilisée contre un gouvernement oppressif.",
+        "effect": {
+            "econ": 0,
+            "civi": 10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Je supporte max stiner.",
+        "effect": {
+            "econ": 0,
+            "civi": 20,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Il faut protéger la liberte.",
+        "effect": {
+            "econ": 0,
+            "civi": 10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "L'autorité est juste excellente.",
+        "effect": {
+            "econ": 0,
+            "civi": -10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "L'etat de transition est genial.",
+        "effect": {
+            "econ": 0,
+            "civi": -10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Supprimons les d*ssident politique.",
+        "effect": {
+            "econ": 0,
+            "civi": -10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Il n'y a pas de réelle oligarchie.",
+        "effect": {
+            "econ": 0,
+            "civi": -10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "L'etat est nécessaire",
+        "effect": {
+            "econ": 0,
+            "civi": -5,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "La dictature a toujours été une mauvaise idée.",
+        "effect": {
+            "econ": 0,
+            "civi": 10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "1984 est une mauvaise chose.",
+        "effect": {
+            "econ": 0,
+            "civi": 10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Les gens ne devraient pas sympathiser avec Hitler.",
+        "effect": {
+            "econ": 0,
+            "civi": 10,
+            "dipl": 0,
+            "soci": 5,
+            "comm": 0
+        }
+    },
+    {
+        "question": "L'économie doit pas être dirigée par l'état.",
+        "effect": {
+            "econ": -5,
+            "civi": 10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "soit tout le monde est roi soit personne ne l'est.",
+        "effect": {
+            "econ": 5,
+            "civi": 10,
+            "dipl": 0,
+            "soci": 5,
+            "comm": 0
+        }
+    },
+    {
+        "question": "La dictature a toujours été une mauvaise idée.",
+        "effect": {
+            "econ": 0,
+            "civi": 10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "La démocratie ne sert à rien.",
+        "effect": {
+            "econ": 0,
+            "civi": -10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "La démocratie rend les gens bêtes .",
+        "effect": {
+            "econ": 0,
+            "civi": -10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Faisont tout pour éviter l'anarchie.",
+        "effect": {
+            "econ": 0,
+            "civi": -10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Je ne sais pas pourquoi les détestent l'autorité.",
+        "effect": {
+            "econ": 0,
+            "civi": -10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
+    {
+        "question": "Quand il y a une manifestation j'ai tendance à me mettre du côté du gouvernement.",
+        "effect": {
+            "econ": 0,
+            "civi": -10,
+            "dipl": 0,
+            "soci": 0,
+            "comm": 0
+        }
+    },
